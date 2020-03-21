4fc4a549dafb96291d3a10510e0768423af2ed78
David Cho <david@peachstreet.com>
2020-03-21 12:30:51 -0400

    Add signed out page for lance

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import PokerTable from "./pokerTable";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SignedOutPage from "./pages/signedOut";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <PokerTable />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/play" component={PokerTable} />
+        <Route path="*" component={SignedOutPage} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ web/src/pages/signedOut/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// TODO: Lance.
+const SignedOutPage = () => {
+  return <div>You are signed out</div>;
+};
+
+export default SignedOutPage;
