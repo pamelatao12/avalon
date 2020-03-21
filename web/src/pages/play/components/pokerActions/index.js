9efda90ad5c64dc4c10e95b00d872e5367a87e40
David Cho <david@peachstreet.com>
2020-03-21 14:28:43 -0400

    Add TODO for anthony for pokerActions

---
+++ web/src/pages/play/components/pokerActions/index.js
@@ -0,0 +1,7 @@
+// TODO: Anthony
+// 1) Add buttons to match https://www.playny.com/wp-content/uploads/2017/12/global4-1024x744.png.
+const PokerActions = () => {
+
+};
+
+export default PokerActions;
