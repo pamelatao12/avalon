a81acf7eb80aec8b8f770e3fa820bd24830e4860
Pamela Tao <pamelatao12@gmail.com>
2020-03-21 14:32:28 -0400

    add user profile component

--- web/src/pages/play/components/player.jsx
+++ web/src/pages/play/components/player.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 import "./player.css";
 import Card from "./card";
+import Profile from "./profile";
 
 const Player = () => {
   const [cards, setCards] = useState([["Q", "Spades"], ["10", "Clubs"]]);
   return (
-    <div className="playerCards">
-      {cards.map((card, i) => (
-        <Card key={i} />
-      ))}
-    </div>
+    <>
+      <div className="playerProfile">
+        <Profile />
+      </div>
+      <div className="playerCards">
+        {cards.map((card, i) => (
+          <Card key={i} number={card[0]} suit={card[1]} />
+        ))}
+      </div>
+    </>
   );
 };
 
