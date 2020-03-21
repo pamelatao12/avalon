983d82a8eb1ac28e3029e24e9fc42853c97598c2
Pamela Tao <pamelatao12@gmail.com>
2020-03-21 16:26:37 -0400

    added 9 players to table

--- web/src/pages/play/components/table/player.jsx
+++ web/src/pages/play/components/table/player.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import "pages/play/components/table/player.css";
 import Card from "pages/play/components/table/card";
 
-const Player = ({ name, pic }) => {
+const Player = ({ name, pic, position }) => {
   const [cards, setCards] = useState([["Q", "Spades"], ["10", "Clubs"]]);
   return (
-    <div className="playerProfile">
-      <div className="playerCards">
+    <div className={`playerProfile${position}`}>
+      <div className={`playerCards${position}`}>
         {cards.map((card, i) => (
           <Card key={i} number={card[0]} suit={card[1]} />
         ))}
--- web/src/pages/play/components/table/pokerTable.jsx
+++ web/src/pages/play/components/table/pokerTable.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "pages/play/components/table/pokerTable.module.css";
 import TableCards from "pages/play/components/table/tableCards";
 import Player from "pages/play/components/table/player";
 
 const PokerTable = () => {
+  const [players, setPlayers] = useState([
+    ["Pamela", "./yay.jpg", 1],
+    ["David", "./yay2.jpg", 2],
+    ["Lance", "./yay3.jpg", 3],
+    ["Peter", "./yay4.jpg", 4],
+    ["Joe", "./yay5.jpg", 5],
+    ["Anthony", "./yay6.jpg", 6],
+    ["Carmen", "./yay7.jpg", 7],
+    ["PlayerX", "./yay8.jpg", 8],
+    ["PlayerY", "./yay9.jpg", 9]
+  ]);
+
   return (
     <div className={styles.tableWrapper}>
       <div className={styles.players}>
-        <Player name="Pamela" pic="./yay.jpg" />
+        {players.map((player, i) => (
+          <Player
+            name={player[0]}
+            pic={player[1]}
+            position={player[2]}
+            key={i}
+          />
+        ))}
       </div>
       <div className={styles.table}>
         <TableCards />
