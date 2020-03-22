af78e248567cb65606142d13f4015af7e252316e
David Cho <david@peachstreet.com>
2020-03-22 14:11:27 -0400

    Only initialize firebase if not already initialized

--- server/src/modules/firebase/db/index.ts
+++ server/src/modules/firebase/db/index.ts
@@ -4,10 +4,14 @@ const serviceAccount = require("../../../../firebase-adminsdk.json");
 
 const DATABASE_URL = "https://degen-poker.firebaseio.com";
 
-firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(serviceAccount),
-  databaseURL: DATABASE_URL
-});
+let isInitialized = false;
+
+if (!isInitialized) {
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount),
+    databaseURL: DATABASE_URL
+  });
+}
 
 export const db = firebaseAdmin.database();
 
