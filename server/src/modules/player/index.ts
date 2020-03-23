0a6480a23229e64a9f32f97d259e7af7a2c129d0
Peter Kang <peter@DV01MACLT19.fios-router.home>
2020-03-23 01:18:41 -0400

    wip

--- server/src/modules/player/index.ts
+++ server/src/modules/player/index.ts
@@ -21,23 +21,22 @@ export type Player = {
  * TODO: Remove default values once we are ready to accept user requests. These
  * were added to make testing easier.
  */
+const player: Player = {
+  currentBetSize: null,
+  holeCards: null,
+  isBigBlind: false,
+  isDealer: false,
+  isFolded: false,
+  isSmallBlind: false,
+  seatState: SeatState.SEATED,
+  stackSize: 2000,
+  userId: "davidvcho@gmail.com"
+};
 
 export const sitAtTable = async (
   tableId: string = "-M33QUXuHYBlHzFJjZ1V",
   seatNumber: number = 1
 ) => {
-  const player: Player = {
-    currentBetSize: null,
-    holeCards: null,
-    isBigBlind: false,
-    isDealer: false,
-    isFolded: false,
-    isSmallBlind: false,
-    seatState: SeatState.SEATED,
-    stackSize: 2000,
-    userId: "davidvcho@gmail.com"
-  };
-
   const committed = await database.setInTransaction(
     `tables/${tableId}/players/${seatNumber}`,
     (currentData: any) => {
@@ -52,16 +51,35 @@ export const sitAtTable = async (
   return committed ? player : undefined;
 };
 
-export const standUp = async () => {
+export const standUp = async (
+  tableId: string = "-M33QUXuHYBlHzFJjZ1V",
+  seatNumber: number = 1
+) => {
+  const committed = await database.setInTransaction(
+    `tables/${tableId}/players/${seatNumber}/seatState`,
+    (currentData: any) =>  {
+      if (currentData === SeatState.SEATED) {
+        return SeatState.STANDING
+      }
+    }
+  )
+  return committed ? SeatState.STANDING : null
   /**
    * TODO: Mark player as standing.
    */
+
 };
 
 export const leaveTable = async (
   tableId: string = "-M33QUXuHYBlHzFJjZ1V",
   seatNumber: number = 1
 ) => {
+  const committed = await database.setInTransaction(
+    `tables/${tableId}/players/${seatNumber}`,
+    () => null
+  )
+  // return true if successfully cleared the seat
+  return committed === null
   /**
    * TODO: Remove player from table.
    */
